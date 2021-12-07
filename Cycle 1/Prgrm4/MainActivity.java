db7df1be88fcf5caa192191d436fa6f790c72049
ashna-jolly <74651041+ashna-jolly@users.noreply.github.com>
2021-12-07 10:37:04 +0530

    Add files via upload

---
+++ Cycle 1/Prgrm4/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.sjcet.bgcolor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1, button2;
+        final RelativeLayout relativeLayout;
+        button1 = findViewById(R.id.btVar1);
+        button2 = findViewById(R.id.btVar2);
+        relativeLayout = findViewById(R.id.rlVar1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                relativeLayout.setBackgroundResource(R.color.colorPrimary);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                relativeLayout.setBackgroundResource(R.color.colorAccent);
+            }
+        });
+    }
+}
